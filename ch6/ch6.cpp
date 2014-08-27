3829182872c439c58f1b77a722034d64dc06d4d9
kudrom <kudrom@riseup.net>
2014-08-27 14:03:10 +0200

    Challenge 6 partially solved

--- ch6/ch6.cpp
+++ ch6/ch6.cpp
@@ -1,6 +1,10 @@
 #include "../includes/hamming.h"
 #include "../includes/base64.h"
+#include "../includes/codec_xor.h"
 #include <cstdio>
+#include <fstream>
+#include <cassert>
+#include <cmath>
 
 int test_hamming_distance(int argc, char *argv[])
 {
@@ -22,12 +26,66 @@ int main(int argc, char *argv[])
         std::cout << "Usage: ch6 <input_file>" << std::endl;
         return -1;
     }
-    std::string input {argv[1]};
-    std::string output;
+    std::string filename {argv[1]};
+    std::string decoded, line;
+    std::ifstream input {filename};
+    int KEYSIZE {40};
+    std::multimap<double, int> keysize_mapping;
 
-    output = base64_decrypt(input);
-
-    for (char c : output){
-        printf("%02X", c);
+    if (input.is_open()){
+        while (std::getline(input, line)){
+            decoded += base64_decrypt(line);
+        }
     }
+
+    for (int i = 2; i < KEYSIZE; i++){
+        std::string str1 {decoded.substr(0, i)};
+        std::string str2 {decoded.substr(i, i)};
+        assert(str1.size() == str2.size());
+
+        int distance {hamming_distance(str1, str2)};
+        double score = (distance / (double) i);
+        keysize_mapping.insert(std::pair<double, int>(score, i));
+    }
+    std::multimap<double, int>::iterator it {keysize_mapping.begin()};
+    std::vector<int> better_keysizes;
+    for (int i = 0; it != keysize_mapping.end(); it++, i++){
+        if (i == 4){
+            break;
+        }
+        better_keysizes.push_back(it->second);
+    }
+
+    for(int keysize : better_keysizes){
+        int blocks = ceil(decoded.size() / keysize);
+        std::vector<std::string> transposed;
+        printf("keysize: %d\n", keysize);
+        for(int i = 0; i < keysize; i++){
+            transposed.push_back(std::string {});
+            for(int block = 0; block < blocks; block++){
+                int index = (block * keysize) + i;
+                if (index < decoded.size()){
+                    char hex[2];
+                    sprintf(hex, "%02X", decoded[index]);
+                    transposed[i] += hex;
+                }else{
+                    transposed[i] += '\0';
+                }
+            }
+        }
+        for(std::string str : transposed){
+            std::multimap<int, struct decoded_string> ranking = probe_keys(str);
+            std::multimap<int, struct decoded_string>::reverse_iterator rit;
+            int limit = 0;
+            for(rit = ranking.rbegin(); rit != ranking.rend(); rit++, limit++){
+                std::cout << rit->second.key;
+                if (limit == 4)
+                    break;
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
